e9e1d23e20fb4e4abfdbe67c981b9381df48bb64
Nikolay <ya.sapunovops2.ru@list.ru>
2021-05-06 05:17:39 +0300

    First MovieDB script

--- js/udemyCourse.js
+++ js/udemyCourse.js
@@ -1,25 +1,58 @@
 "use strict";
 
-udemyCourseApp();
+setTimeout(() => {
+    udemyCourseApp();
+}, 1000);
 
 function udemyCourseApp() {
     const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", 10);
+    if (numberOfFilms != "" && numberOfFilms != null && numberOfFilms >= 1) {
+        const personalMovieDB = {
+            count: numberOfFilms,
+            movies: {},
+            actors: {},
+            genres: [],
+            private: false,
+        };
+        if (personalMovieDB.count < 10) {
+            alert("Просмотрено довольно мало фильмов");
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
+            alert("Вы классический зритель");
+        } else {
+            alert("Вы киноман.");
+        }
 
-    const personalMovieDB = {
-        count: numberOfFilms,
-        movies: {},
-        actors: {},
-        genres: [],
-        private: false,
-    };
+        for (let i = 0; i < 2; i++) {
+            const latestMovie = prompt("Один из последних просмотренных фильмов?");
+            if (latestMovie == "") {
+                alert("Вы не ввели название фильма.");
+                i--;
+            } else if (latestMovie == null) {
+                alert("Вы не ответили на вопрос.");
+                i--;
+            } else if (latestMovie.length > 50) {
+                alert("Слишком длинное название фильма. Попробуйте ещё раз.");
+                i--;
+            } else {
+                for (let i = 0; i < 1; i++) {
+                    const latestMovieRating = prompt("На сколько оцените его?");
 
-    const firstLatestMovie = prompt("Один из последних просмотренных фильмов?"),
-        firstLatestMovieRating = prompt("На сколько оцените его?"),
-        secondLatestMovie = prompt("Один из последних просмотренных фильмов?"),
-        secondLatestMovieRating = prompt("На сколько оцените его?");
-
-    personalMovieDB.movies[firstLatestMovie] = firstLatestMovieRating;
-    personalMovieDB.movies[secondLatestMovie] = secondLatestMovieRating;
-
-    console.log(personalMovieDB);
+                    if (latestMovieRating == "") {
+                        alert("Вы не указали оценку для фильма.");
+                        i--;
+                    } else if (latestMovieRating == null) {
+                        alert("Вы не ответили на вопрос.");
+                        i--;
+                    } else {
+                        personalMovieDB.movies[latestMovie] = latestMovieRating;
+                        console.log(personalMovieDB);
+                        alert(`Фильм: ${latestMovie}, Оценка: ${latestMovieRating}`);
+                    }
+                }
+            }
+        }
+    } else {
+        alert("Ошибка. Повторите ввод.");
+        udemyCourseApp();
+    }
 }
